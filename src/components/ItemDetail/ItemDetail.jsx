77584e8dc2e4e62cd3a3e7e34a81087eb48b4be8
Maximiliano Saldivia <saldiviamaximiliano2@gmail.com>
2022-06-15 15:08:51 -0300

    Item detail responsive.

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -20,36 +20,105 @@ function ItemDetail({product, loading}) {
   // console.log("product", product)
   
   return (
-    <Container sx={{mt: "5%"}} className='CONTAINER' maxWidth="lg" align="center">
-    {loading ? 
-      <Loading />
-      :
-      product.length !== 0 ?
-      <Box className='AAAAAAAAA' justifyContent="center">
-       
-       <Button 
-          startIcon={<ShoppingCartIcon />} 
-          sx={{mb: 2, width: "250px" }} 
-          variant="contained" 
-          color="success"
-        >
-          <Link to={"/"} style={{textDecoration: "none", color:"#fff"}}>
-            Seguir comprando
-          </Link>
-        </Button>
-          
-        <Grid container sx={{ width: "80%", height: "100%" , bgcolor: "#000", alignItems: "center"}}>
-          <Grid item xs={12} sm={12} md={6} component="img" src={product.pictureUrl} sx={{maxHeight: "300px"}} />
-          <Grid item xs={12} sm={12} md={6} component="div" sx={{p: 4}}>
-            <Typography gutterBottom variant="h5" component="div" sx={{color: "#94ff8f", display:"block", fontWeight: "700"}}>{product.title}</Typography>
-            <Typography gutterBottom variant="h7" component="div" sx={{color: "#fff", display: "block"}}><Typography variant="h6" sx={{fontWeight: "600"}}>Ingredientes:</Typography> {product.description}</Typography>
-            <Typography gutterBottom variant='h6' component="div" sx={{color: "#fff", display:"block", fontWeight: "700"}}>${product.price}</Typography>
-            <ItemCount stock={product.stock} initial={1} addItemToCart={addItemToCart} product={product}/>
-          </Grid>
-        </Grid>
-      </Box>
-      : <GenericError message={"Error al cargar el detalle del producto"} />
-    }
+    <Container 
+      sx={{mt: "5%"}}
+      className='CONTAINER' 
+      maxWidth="lg" 
+      align="center"
+    >
+      {loading ? 
+        <Loading />
+        :
+        product.length !== 0 ?
+        <Box  justifyContent="center">
+        
+          <Button 
+            startIcon={<ShoppingCartIcon />} 
+            sx={{mb: 2, width: "250px" }} 
+            variant="contained" 
+            color="success"
+            >
+              <Link 
+                to={"/"} 
+                style={{textDecoration: "none", color:"#fff"}}
+              >
+                  Seguir comprando
+              </Link>
+          </Button>
+              
+            <Grid 
+              container 
+              sx={{ 
+                width: "80%", 
+                height: "100%" , 
+                bgcolor: "#000", 
+                alignItems: "center"
+                }}
+              >
+              <Grid 
+                item 
+                xs={12} sm={12} md={6} 
+                component="img" 
+                src={product.pictureUrl} 
+                sx={{maxHeight: "300px"}} 
+              />
+              <Grid 
+                item 
+                xs={12} sm={12} md={6} 
+                component="div" 
+                sx={{p: 4}}
+              >
+                <Typography 
+                  gutterBottom 
+                  variant="h5" 
+                  component="div" 
+                  sx={{
+                    color: "#94ff8f", 
+                    display:"block", 
+                    fontWeight: "700"
+                  }}>
+                    {product.title}
+                </Typography>
+                <Typography 
+                  gutterBottom 
+                  variant="h7" 
+                  component="div" 
+                  sx={{
+                    color: "#fff", 
+                    display: "block"
+                  }}
+                >
+                <Typography 
+                  variant="h6" 
+                  sx={{fontWeight: "600"}}
+                  >
+                    Ingredientes:
+                  </Typography> 
+                    {product.description}
+                  </Typography>
+                <Typography 
+                  gutterBottom 
+                  variant='h6' 
+                  component="div" 
+                  sx={{
+                    color: "#fff", 
+                    display:"block", 
+                    fontWeight: "700"
+                    }}
+                >
+                    ${product.price}
+                </Typography>
+                <ItemCount 
+                  stock={product.stock} 
+                  initial={1} 
+                  addItemToCart={addItemToCart} 
+                  product={product}
+                />
+              </Grid>
+            </Grid>
+        </Box>
+        : <GenericError message={"Error al cargar el detalle del producto"} />
+      }
    
     </Container>
   )
